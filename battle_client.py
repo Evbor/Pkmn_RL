f3fcede0aeba72505091428d730301c1b15f230e
Evan <23489366+Evbor@users.noreply.github.com>
2021-08-11 22:55:24 -0700

    first commit

---
+++ battle_client.py
@@ -0,0 +1,116 @@
+import asyncio
+import websockets
+import requests
+import json
+
+class BattleClient:
+    """
+    Battle Client Object, binds to a battle, either a simulated one using a
+    subprocess, or a real battle on a Showdown Server. Provides a public API,
+    update_battle, to update the state of the battle given a battle action.
+
+    Attributes
+    ----------
+    real {Bool}: Indicates if the battle is against a real person
+
+    Methods
+    -------
+    update_battle(battle_action {String})-->List(String): updates battle
+    """
+
+    def __init__(self, team, username, showdown_uri=None, password=None):
+        if showdown_uri and password:
+            self.real = True
+            self.websocket = None
+            self.loop = asyncio.get_event_loop() # asyncio loop
+            self.loop.run_until_complete(
+                self.__init_real_battle(showdown_uri, username, password, team)
+                )
+        elif not showdown_uri and not password:
+            self.real = False
+            self.battle = self.__init_sim_battle(username, team)
+        else:
+            raise ValueError('both showdown_url and password must be provided'
+                             'or left as None')
+
+    def update_battle(self, battle_action):
+        """
+        Updates the battle the client is bound to by having the active Pokemon
+        perform :param battle_action:. Returns the updated battle state as a
+        list of messages. For types of actions an active Pokemon could take
+        see: https:
+
+        :param battle_action {String}: command active Pokemon should execute
+
+        ---> messages {List(String)}, a list of battle messages reflecting the
+                                      current battle state
+        """
+
+        raise NotImplementedError
+
+    async def send_messages(self, room, messages):
+        """
+        Async task used send messages to the Pokemon Showdown server over a
+        websocket.
+
+        :param room {String}: room name of the chat room to send the messages to
+        :param messages {List(String)}: list of messages to pass to the
+                                        specified room on the server
+
+        ---> None
+        """
+
+        message = '{}|{}'.format('|'.join(messages))
+        await self.websocket.send(message)
+
+    async def __login(self, username, password, challstr):
+        """
+        Async login task.
+        """
+
+        payload = {
+            'act': 'login',
+            'name': username,
+            'pass': password,
+            'challstr': challstr
+            }
+        r = requests.post('https://play.pokemonshowdown.com/action.php',
+            data=payload
+            )
+        # asserting correct response
+        assert r.text[0] == ']'
+
+        data = json.loads(r.text[1:])
+        # assert login went successfully
+        assert data['curuser']['loggedin'] and data['assertion'][0:2] != ';;'
+
+        assertion = data['assertion']
+        await __send_messages(room, messages)
+
+
+    async def __init_real_battle(self, showdown_uri, username, password, team):
+        """
+        Creates a random battle on Pokemon Showdown server :param showdown_url:
+        with user :param username: and Pokemon team :param team:.
+
+        :param showdown_url {String}: URL of Pokemon Showdown server
+        :param username {String}: username of user on server to login as
+        :param password {String}: password of user
+        :param team {String}: Pokemon team to use in format (...)
+
+        ---> battle {websockets.WebSocketClientProtocol}, battle object for
+                                                          real battles
+        """
+
+        self.websocket = await websockets.connect(showdown_uri)
+        async for message in self.websocket:
+            # if message is challstr ==> login given user
+            if '|challstr|' == message[0:10]:
+                await __login(username, password, message[10:])
+
+    def __init_sim_battle(self, username, team):
+        """
+        Creates a Pokemon battle subprocess using the Pokemon Showdown battle
+        simulator tool.
+        """
+        pass
---
+++ tests/test_battle_client.py
@@ -0,0 +1,68 @@
+import unittest
+import subprocess
+from unittest.mock import patch
+from battle_client import BattleClient
+
+class TestBattleClientReal(unittest.IsolatedAsyncioTestCase):
+    """
+    Test case for BattleClient object binding to a real battle
+    """
+
+    def setUp(self):
+        # BattleClient configuration
+        self.team = 'test_team'
+        self.username = 'blahahahaXDRaR'
+        self.showdown_uri = 'ws://localhost:8000/showdown/websocket'
+        self.password = '1Parrot1!'
+        # Starting local Pokemon Showdown server
+        self.ps = subprocess.Popen(['./pokemon-showdown/pokemon-showdown'],
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+                )
+
+    @patch.object(BattleClient, '_BattleClient__init_real_battle')
+    def test_init_realness(self, mock_BattleClient):
+        battle = BattleClient(self.team, self.username, self.showdown_uri,
+                self.password
+                )
+        self.assertTrue(battle.real)
+
+    @patch.object(BattleClient, '_BattleClient__init_real_battle')
+    def test_init_send_messages(self, mock_BattleClient):
+        pass
+
+    def tearDown(self):
+        # Destroying BattleClient Config
+        del self.team
+        del self.username
+        del self.showdown_uri
+        del self.password
+        # Killing local Pokemon Showdown server
+        try:
+            self.ps.terminate()
+            self.ps.communicate(timeout=0.2)
+        except subprocess.TimeoutExpired:
+            print('Server did not terminate in time')
+            self.ps.kill
+
+        del self.ps
+
+class TestBattleClientSim(unittest.TestCase):
+    """
+    Test case for BattleClient object binding to a simulated battle
+    """
+
+    def setUp(self):
+        self.team = 'test_team'
+        self.username = 'test_username'
+
+    @patch.object(BattleClient, '_BattleClient__init_sim_battle')
+    def test_init_realness(self, mock_BattleClient):
+        battle = BattleClient(self.team, self.username)
+        self.assertFalse(battle.real)
+
+    def tearDown(self):
+        del self.team
+        del self.username
+
+if __name__ == '__main__':
+    unittest.main()
